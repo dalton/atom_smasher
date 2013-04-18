a8d5e0821abe84f0e90a1e6a8aac3d0390b57868
Andrew Davis <andrew@andrewrdavis.net>
2013-04-18 11:59:38 -0300

    Refactor Feed Tests

--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -1,18 +1,12 @@
 require 'spec_helper'
 describe AtomSmasher::Feed do
   AtomSmasher::Feed.create(url: "#{File.dirname(__FILE__)}/../fixtures/previous_episodes.xml")
-  it "should be have all episodes" do
-    feed = AtomSmasher::Feed.last
-    feed.should have(9).posts
-  end
-
-  it "should get the title from the feed" do
-    feed = AtomSmasher::Feed.last
-    feed.title.should == "ASCIIcasts - Previous Episodes"
-  end
+  
+  subject(:feed) { AtomSmasher::Feed.last }
+  it { should have(9).posts }
+  its(:title) { should == "ASCIIcasts - Previous Episodes" }
 
   it "should update if a new item is posted" do
-    feed = AtomSmasher::Feed.last
     feed.url =  "#{File.dirname(__FILE__)}/../fixtures/current_episodes.xml"
     feed.save
     feed = AtomSmasher::Feed.last
